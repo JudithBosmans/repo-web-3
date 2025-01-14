f30743095db4b9da567ef20cbb4e45bd4f0e8fba
Judith Bosmans <judobosmans@gmail.com>
2025-01-14 14:47:52 +0100

    update - hamburger

--- components/ColorMode.vue
+++ components/ColorMode.vue
@@ -1,58 +1,102 @@
 <template>
   <div>
-  <client-only>
-    
-    <div class="icon-container">
-      <button
-      class="icon"
-        :class="{ active: colorMode.preference === 'light' }"
-        @click="setColorMode('light')"
+    <client-only>
+      <div class="icon-container">
+        <button
+          class="icon"
+          :class="{ active: colorMode.preference === 'light' }"
+          @click="setColorMode('light')"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 21.154q-.69 0-1.201-.463t-.607-1.152h3.616q-.096.69-.607 1.152T12 21.154m-3.5-3.385v-1h7v1zM8.558 15q-1.417-.929-2.238-2.356T5.5 9.5q0-2.721 1.89-4.61T12 3t4.61 1.89T18.5 9.5q0 1.717-.82 3.144T15.442 15zm.292-1h6.3q1.125-.8 1.738-1.975T17.5 9.5q0-2.3-1.6-3.9T12 4T8.1 5.6T6.5 9.5q0 1.35.613 2.525T8.85 14M12 14"></path></svg>
-      </button>
-      <button
-      class="icon"
-        :class="{ active: colorMode.preference === 'dark' }"
-        @click="setColorMode('dark')"
+          <svg
+            class="w-6 h-6"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+            />
+          </svg>
+        </button>
+        <button
+          class="icon"
+          :class="{ active: colorMode.preference === 'dark' }"
+          @click="setColorMode('dark')"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 21.154q-.69 0-1.201-.463t-.607-1.152h3.616q-.096.69-.607 1.152T12 21.154M12 4q-1.12 0-2.124.426t-1.73 1.14l-.719-.708q.986-.912 2.116-1.385T12 3q2.721 0 4.61 1.89T18.5 9.5q0 1.506-.538 2.605t-1.358 1.956l-.708-.707q.583-.575 1.094-1.543q.51-.967.51-2.311q0-2.3-1.6-3.9T12 4m8.162 16.43l-.714.714L13.304 15H8.558q-1.417-.929-2.238-2.356T5.5 9.5q0-.52.091-1.08q.092-.562.236-.897l-3.158-3.17l.708-.707zM8.85 14h3.454l-5.65-5.65q-.058.223-.106.546T6.5 9.5q0 1.35.613 2.525T8.85 14m6.685 2.77v1H8.5v-1z"></path></svg>
-      </button>
-      <button
-      class="icon"
-        :class="{ active: colorMode.preference === 'system' }"
-        @click="setColorMode('system')"
+          <svg
+            class="w-6 h-6"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+        <!-- <button
+          class="icon"
+          :class="{ active: colorMode.preference === 'system' }"
+          @click="setColorMode('system')"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 19v-2H4.616q-.691 0-1.153-.462T3 15.385v-9.77q0-.69.463-1.152T4.615 4h14.77q.69 0 1.152.463T21 5.616v9.769q0 .69-.463 1.153T19.385 17H13v2h1.5q.214 0 .357.143T15 19.5t-.143.357T14.5 20h-5q-.213 0-.357-.143T9 19.5t.143-.357T9.5 19zm-6.384-3h14.769q.23 0 .423-.192t.192-.423v-9.77q0-.23-.192-.423T19.385 5H4.615q-.23 0-.423.192T4 5.616v9.769q0 .23.192.423t.423.192M4 16V5z"></path></svg>
-      </button>
-      <button
-      class="icon"
-        :class="{ active: colorMode.preference === 'gray' }"
-        @click="setColorMode('gray')"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M11 19v-2H4.616q-.691 0-1.153-.462T3 15.385v-9.77q0-.69.463-1.152T4.615 4h14.77q.69 0 1.152.463T21 5.616v9.769q0 .69-.463 1.153T19.385 17H13v2h1.5q.214 0 .357.143T15 19.5t-.143.357T14.5 20h-5q-.213 0-.357-.143T9 19.5t.143-.357T9.5 19zm-6.384-3h14.769q.23 0 .423-.192t.192-.423v-9.77q0-.23-.192-.423T19.385 5H4.615q-.23 0-.423.192T4 5.616v9.769q0 .23.192.423t.423.192M4 16V5z"
+            ></path>
+          </svg>
+        </button>
+        <button
+          class="icon"
+          :class="{ active: colorMode.preference === 'gray' }"
+          @click="setColorMode('gray')"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6.5 19q-1.871 0-3.185-1.306Q2 16.39 2 14.517q0-1.719 1.175-3.051t2.921-1.431q.337-2.185 2.01-3.61T12 5q2.502 0 4.251 1.749T18 11v1h.616q1.436.046 2.41 1.055T22 15.5q0 1.471-1.014 2.486Q19.97 19 18.5 19zm0-1h12q1.05 0 1.775-.725T21 15.5t-.725-1.775T18.5 13H17v-2q0-2.075-1.463-3.538T12 6T8.463 7.463T7 11h-.5q-1.45 0-2.475 1.025T3 14.5t1.025 2.475T6.5 18m5.5-6"></path></svg>
-      </button>
-    </div>
-    
-    <div class="w-full mb-5">
-        <p class="description">Color mode: {{ colorMode.preference }}
-        </p>
-    </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M6.5 19q-1.871 0-3.185-1.306Q2 16.39 2 14.517q0-1.719 1.175-3.051t2.921-1.431q.337-2.185 2.01-3.61T12 5q2.502 0 4.251 1.749T18 11v1h.616q1.436.046 2.41 1.055T22 15.5q0 1.471-1.014 2.486Q19.97 19 18.5 19zm0-1h12q1.05 0 1.775-.725T21 15.5t-.725-1.775T18.5 13H17v-2q0-2.075-1.463-3.538T12 6T8.463 7.463T7 11h-.5q-1.45 0-2.475 1.025T3 14.5t1.025 2.475T6.5 18m5.5-6"
+            ></path>
+          </svg>
+        </button> -->
+      </div>
 
-
-
-
-  </client-only>
+      <div class="w-full mb-5">
+        <p class="description">{{ colorMode.preference }} mode</p>
+      </div>
+    </client-only>
   </div>
 </template>
 
 <script setup lang="ts">
 // Initialize colorMode
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
 // Method to set the color mode preference
 const setColorMode = (mode: string) => {
-  colorMode.preference = mode
-}
+  colorMode.preference = mode;
+};
 </script>
 
 <style scoped>
@@ -65,7 +109,14 @@ const setColorMode = (mode: string) => {
   font-size: 2em;
   transition: color 0.3s;
 }
+
+.icon:hover {
+  color: red;
+  fill: red;
+}
+
 .icon.active {
-  color: var(--color-active); /* Define this variable in your global CSS */
+  /*color: var(--color-active);  Define this variable in your global CSS */
+  color: red;
 }
 </style>
--- components/Drawer.vue
+++ components/Drawer.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="flex animate-fade animate-once animate-delay-[500ms]">
     <div class="flex-auto w-1 pt-3"></div>
-    <div class="flex-row pt-5 justify-end">
-      <button style="margin-left: 16px" @click="drawer = true" class="bg-white dark:bg-slate-900 bg-opacity-20 hover:bg-opacity-90 dark:bg-opacity-10">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M4.5 17.27q-.213 0-.356-.145T4 16.768t.144-.356t.356-.143h15q.213 0 .356.144q.144.144.144.357t-.144.356t-.356.143zm0-4.77q-.213 0-.356-.144T4 11.999t.144-.356t.356-.143h15q.213 0 .356.144t.144.357t-.144.356t-.356.143zm0-4.77q-.213 0-.356-.143Q4 7.443 4 7.23t.144-.356t.356-.143h15q.213 0 .356.144T20 7.23t-.144.356t-.356.144z"></path>
+    <!-- Parent container with relative positioning -->
+    <div class="flex-row pt-5 justify-end relative">
+      <!-- Sticky button -->
+      <button
+        @click="drawer = true"
+        class="bg-opacity-20 hover:bg-opacity-90 dark:bg-opacity-10 sticky top-4"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1.5em"
+          height="1.5em"
+          viewBox="0 0 24 24"
+          class="m-4"
+        >
+          <path
+            class="iconNav"
+            d="M4.5 17.27q-.213 0-.356-.145T4 16.768t.144-.356t.356-.143h15q.213 0 .356.144q.144.144.144.357t-.144.356t-.356.143zm0-4.77q-.213 0-.356-.144T4 11.999t.144-.356t.356-.143h15q.213 0 .356.144t.144.357t-.144.356t-.356.143zm0-4.77q-.213 0-.356-.143Q4 7.443 4 7.23t.144-.356t.356-.143h15q.213 0 .356.144T20 7.23t-.144.356t-.356.144z"
+          ></path>
         </svg>
       </button>
     </div>
@@ -12,20 +26,23 @@
 
   <el-drawer
     v-model="drawer"
-    class="drawer min-w-fit max-w-screen"
+    class="drawer min-w-[100vw] p-10"
     :direction="direction"
     :custom-class="'fixed-drawer'"
   >
     <!-- Drawer content goes here -->
     <div class="flex flex-col h-full">
-      <div>
-        <Titleblock />
+      <div class="flex justify-center items-center h-full w-full">
+        <div class="text-center">
+          <button
+            @click="navigateToAbout"
+            class="text-2xl lg:text-5xl navText cursor-pointer"
+          >
+            About
+          </button>
+          <Navigations />
+        </div>
       </div>
-      <div class="flex-1 overflow-auto">
-        <Navigations />
-      </div>
-
-      <!-- Footer is placed at the bottom of the drawer -->
       <div class="mt-auto footer">
         <Footer />
         <UtilNav />
@@ -35,30 +52,26 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import type { DrawerProps } from 'element-plus'
-const drawer = ref(false)
-const direction = ref<DrawerProps['direction']>('ltr')
+import { ref } from "vue";
+import type { DrawerProps } from "element-plus";
+import Navigations from "./Navigations.vue";
+import { useRouter } from "vue-router";
+
+const drawer = ref(false);
+const direction = ref<DrawerProps["direction"]>("rtl");
+
+const router = useRouter();
+
+const navigateToAbout = () => {
+  router.push({ path: "/", hash: "#about" }).then(() => {
+    drawer.value = false;
+  });
+};
 </script>
 
 <style>
-.fixed-drawer {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 1000; /* Ensures it appears on top */
-  background-color: rgba(255, 255, 255, 0.99);
-}
-
-.flex-row {
-  display: flex;
-  justify-content: flex-end;
-}
-
 .flex-row button {
-  align-self: flex-start; /* Align the button at the top */
+  align-self: flex-end;
 }
 
 .mt-auto {
